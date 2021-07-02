dd951425a34eb7b44910f2cf6b8d97a085b2cfc7
Kristine Jencite <kjencite@gmail.com>
2021-07-02 18:26:40 +0300

    Forluma1

--- src/Group2/Formulas.java
+++ src/Group2/Formulas.java
@@ -8,5 +8,12 @@ public class Formulas {
     } esle {
         travePrice = (generalprice * adults) + (childrenprice * childrens) + (tripDays* bulkprice)
     }*/
-
+    //MAIN LAPA
+    // System.out.println("Do you have CovidPass? y/n");
+    // covidPassInput = scanner.next().charAt(0);
+//if (covidPassInput = covidpass) {
+    //System.out.println("You are allowed to travel to" + inputCountry)
+    // } else {
+    //System.out.println("Sorry, but without CovidPAss you are not able travel to" + inputCountry)
+//    }
 }
